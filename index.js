fedf2d1a6734fbddf34a7bef25ac43b4335254eb
luisafalcao <luisafalcaovaz@gmail.com>
2023-01-30 17:19:07 -0300

    Implement computer's smart plays

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 let allEmptySquares = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-const squares = document.querySelectorAll('.square')
-const arrayOfSquares = Array.from(squares)
+const squares = Array.from(document.querySelectorAll('.square'))
 const message = document.querySelector('.message')
-
 let completed
 
 const checkContent = () => {
-    const leftColumn = [arrayOfSquares[0].textContent, arrayOfSquares[3].textContent, arrayOfSquares[6].textContent]
-    const centerColumn = [arrayOfSquares[1].textContent, arrayOfSquares[4].textContent, arrayOfSquares[7].textContent]
-    const rightColumn = [arrayOfSquares[2].textContent, arrayOfSquares[5].textContent, arrayOfSquares[8].textContent]
-
-    const topRow = [arrayOfSquares[0].textContent, arrayOfSquares[1].textContent, arrayOfSquares[2].textContent]
-    const centerRow = [arrayOfSquares[3].textContent, arrayOfSquares[4].textContent, arrayOfSquares[5].textContent]
-    const bottomRow = [arrayOfSquares[6].textContent, arrayOfSquares[7].textContent, arrayOfSquares[8].textContent]
-
-    const topRightDiagonal = [arrayOfSquares[0].textContent, arrayOfSquares[4].textContent, arrayOfSquares[8].textContent]
-    const topLeftDiagonal = [arrayOfSquares[2].textContent, arrayOfSquares[4].textContent, arrayOfSquares[6].textContent]
-
+    const leftColumn = [squares[0].textContent, squares[3].textContent, squares[6].textContent]
+    const centerColumn = [squares[1].textContent, squares[4].textContent, squares[7].textContent]
+    const rightColumn = [squares[2].textContent, squares[5].textContent, squares[8].textContent]
+    
+    const topRow = [squares[0].textContent, squares[1].textContent, squares[2].textContent]
+    const centerRow = [squares[3].textContent, squares[4].textContent, squares[5].textContent]
+    const bottomRow = [squares[6].textContent, squares[7].textContent, squares[8].textContent]
+    
+    const topRightDiagonal = [squares[0].textContent, squares[4].textContent, squares[8].textContent]
+    const topLeftDiagonal = [squares[2].textContent, squares[4].textContent, squares[6].textContent]
+    
     let allContent = [leftColumn, centerColumn, rightColumn, topRow, centerRow, bottomRow, topLeftDiagonal, topRightDiagonal]
-
+   
     completed = allContent.some(item => {
         return item.every(element => {
             return element === item[0] && item[0] !== ""
@@ -35,7 +33,7 @@ const checkContent = () => {
         message.classList.add('active')
 
         if (completedLine[0].includes('X')) {
-            message.textContent = 'Good job! You won!'
+            message.textContent = 'Good job! You won! Click here to restart'
         } else if (completedLine[0].includes('O')) {
             message.textContent = 'Oops, we won...'
         }
@@ -44,19 +42,33 @@ const checkContent = () => {
     }
 }
 
-const getRandomNumber = () => {
-    const randomNumber = Math.floor(Math.random() * allEmptySquares.length)
-    return allEmptySquares[randomNumber]
+const getRandomNumber = (array) => {
+    const randomNumber = Math.floor(Math.random() * array.length)
+    return array[randomNumber]
 }
 
-const getComputerSelection = () => {
-    const currentSelectionIndex = getRandomNumber()
+const getComputerSelection = (index) => {
+    const smartPlays = {
+        0: [0, 1, 2, 3, 4, 6, 8],
+        1: [0, 1, 2, 4, 7],
+        2: [0, 1, 2, 4, 5, 6, 8],
+        3: [0, 3, 4, 5, 6],
+        4: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+        5: [2, 3, 4, 5, 8],
+        6: [0, 2, 3, 4, 6, 7, 8],
+        7: [1, 4, 6, 7, 8],
+        8: [0, 2, 4, 5, 6, 7, 8]
+    }
+
+    const possiblePlays = allEmptySquares.filter(element => smartPlays[index].includes(element))
+
+    const currentSelectionIndex = getRandomNumber(possiblePlays)
     
     // remove respective item from allEmptySquares
     allEmptySquares.splice(allEmptySquares.indexOf(currentSelectionIndex), 1)
     
     // loop through squares to find the square that corresponds to the random index
-    const computerSelection = arrayOfSquares.filter((square, index) => {
+    const computerSelection = squares.filter((square, index) => {
         return index === currentSelectionIndex
     })
 
@@ -84,13 +96,13 @@ const getUserSelection = (userSelection, currentIndex) => {
         // get computer selection after 1 second
         setTimeout(() => {
             // get computer selection based on random number
-            getComputerSelection()
+            getComputerSelection(currentIndex)
         }, 1000)
     }
 
 }
 
-arrayOfSquares.map((square, index) => {
+squares.map((square, index) => {
     square.addEventListener('click', () => {
         // get user selection based on user's click
         getUserSelection(square, index)
@@ -101,7 +113,7 @@ message.addEventListener('click', () => {
     // reset
     message.classList.remove('active')
     allEmptySquares = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-    arrayOfSquares.map(square => square.textContent = "")
+    squares.map(square => square.textContent = "")
 
     completed = !completed
 })
